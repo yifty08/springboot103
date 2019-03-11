3b391c36e59b67fffa00afba325ab3c2db2c9b90
yifty08 <yifty08@icloud.com>
2019-03-11 15:21:30 -0400

    final

---
+++ src/main/java/com/example/springboot103/Springboot103Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot103;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot103Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot103Application.class, args);
+	}
+
+}
